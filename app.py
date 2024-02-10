dde813237e21541006a4e4236af1dee7c69b7b1e
Kusum Gandham <koolkusum10@gmail.com>
2024-02-10 16:51:33 -0500

    slay

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from imp import init_builtin
-# from os import urandom
-# from flask import Flask, render_template, redirect, request, session, url_for
-# import sqlite3
-# import google.generativeai as genai
+from os import urandom
+from flask import Flask, render_template, redirect, request, session, url_for
+import sqlite3
+import google.generativeai as genai
 
 
-# app = Flask(__name__)
-# app.secret_key = urandom(24)
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
+app = Flask(__name__)
+app.secret_key = urandom(24)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
-# DATABASE = 'task.db'
+DATABASE = 'task.db'
 
 
 from os import urandom
