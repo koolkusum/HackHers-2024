76ab33e0d97c75bcb3eb1b955ccf5b72bf69763e
Kusum Gandham <koolkusum10@gmail.com>
2024-02-10 16:33:44 -0500

    added import for ai and some basic flask set up

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from os import urandom
 from flask import Flask, render_template, redirect, request, session, url_for
 import sqlite3
+import google.generativeai as genai
+
+
+app = Flask(__name__)
+app.secret_key = urandom(24)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+DATABASE = 'task.db'
+
+@app.route("/")
+def index():
+    return render_template("signup.html")
 
