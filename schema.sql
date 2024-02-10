d2de0f8af359526d4e9b49f7250366fc90633ccd
Kusum Gandham <koolkusum10@gmail.com>
2024-02-10 15:53:29 -0500

    sql schema set up

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    passw TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+    TID INTEGER PRIMARY KEY AUTOINCREMENT,
+    uid INTEGER NOT NULL,
+    task TEXT NOT NULL,
+    date Timestamp NOT NULL
+);
